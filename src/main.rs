fdca6ec76744cce7838a9c5ffd672cb5c6c20396
HMate <mhidvegi@gmail.com>
2019-05-23 22:40:34 +0200

    add choosing game option. There is aaalways a choice.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,54 @@
-
-
 fn main() {
+    show_greeting_messages();
+
+    show_game_options();
+    loop {
+        match choose_game_option() {
+            None => {
+                println!("Hm nice try, but I'm afraid I don't know what you mean.")
+            }
+            Some(option) => {
+                match option {
+                    GameOption::Start => println!("That was a good ride."),
+                    GameOption::Exit => {
+                        println!("See you soon! Take care of yourself Tiger 😉");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn show_greeting_messages() {
     println!("Welcome dear visitor in the worlds most ambitious, most violent, most demanding game .. OF .. RUMOEBA!");
-
     println!("But first things first. Please humble me with giving your first choice.");
-    let mut user_input = String::new();
-    std::io::stdin().read_line(&mut user_input);
+}
 
-    println!("Hello, world! {}", user_input);
+enum GameOption {
+    Start,
+    Exit,
+}
+
+impl std::str::FromStr for GameOption {
+    type Err = u32;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &s[..] {
+            "1" => Ok(GameOption::Start),
+            "2" => Ok(GameOption::Exit),
+            _ => Err(2)
+        }
+    }
+}
+
+fn show_game_options() {
+    println!("1) Start RUMOEBA!");
+    println!("2) Exit");
+}
+
+fn choose_game_option() -> Option<GameOption> {
+    let mut user_input = String::new();
+    std::io::stdin().read_line(&mut user_input).unwrap();
+    user_input.trim().parse::<GameOption>().ok()
 }
