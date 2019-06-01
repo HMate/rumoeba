b19450260a14f8dfd4436c5a9eb7c02da28e51d3
HMate <mhidvegi@gmail.com>
2019-06-01 16:38:19 +0200

    Lets play this game. Is it already finished?

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,42 @@
 use crate::game_board::BoardSizeT;
 
 pub mod ui;
-pub mod game_board;
+mod game_board;
+
+const MIN_BOARD_SIZE: BoardSizeT = 3;
+const MAX_BOARD_SIZE: BoardSizeT = 25;
+
+pub struct XOGame {
+
+}
+
+impl XOGame {
+    pub fn new(board_size: BoardSizeT, player_uno: &Player, player_dos: &Player) -> XOGame {
+        return XOGame{};
+    }
+
+    pub fn place(&self, player: &Player, x: BoardSizeT, y: BoardSizeT) {
+
+    }
+
+    pub fn finished(&self) -> bool {
+        true
+    }
+}
+
+pub struct Player {
+
+}
+
+impl Player {
+    pub fn new(name: &str) -> Player {
+        return Player{};
+    }
+}
+
 
 pub fn start_game() {
-    ui::show_message("Pick a board size!");
+    ui::show_message("What dimensions are we talking about?");
     let board_size = read_board_size();
 
     ui::show_message(&format!("Let the game begin! {}", board_size));
@@ -21,7 +53,7 @@ fn read_board_size() -> BoardSizeT {
             continue;
         }
         if let Ok(parsed_number) = user_input.trim().parse::<BoardSizeT>() {
-            if 2 < parsed_number && parsed_number < 25 {
+            if MIN_BOARD_SIZE <= parsed_number && parsed_number < MAX_BOARD_SIZE {
                 return parsed_number;
             }
         }
